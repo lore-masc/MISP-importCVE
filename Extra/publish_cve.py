052b6c99b35aaab6812863c19342e73235d86066
lore-masc <lorenzo.masciullo@studenti.unitn.it>
2018-03-30 12:08:51 +0200

    publish cve script

---
+++ Extra/publish_cve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from pymisp import PyMISP
+import json
+import sys
+sys.path.append('../')
+from keys import misp_url, misp_key
+
+def init(url, key):
+    return PyMISP(url, key, False, 'json', debug=False)
+
+misp = init(misp_url, misp_key)
+
+result = misp.search_index(published=0)
+#print(json.dumps(result['response']))
+
+for cve in result['response']:
+	cve_id = cve['id']
+	event = misp.get_event(cve_id)
+	if event['Event']['published'] == False:
+		cve_info = event['Event']['info']
+		misp.fast_publish(cve_id)
+		print(cve_info + " published\n")
