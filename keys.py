8c6a7741eedd34d4a7b66344339dabeb47666566
lore-masc <lorenzo.masciullo@studenti.unitn.it>
2018-04-09 10:43:21 +0200

    keys.py deleted

--- keys.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-misp_url = 'https://devmisp01.noc.tnnet.it'
-misp_key = 'xXM4uCm1Uvsm3OMIhkQPqZRgnFSWhdyl0qBth09Y' # The MISP auth key can be found on the MISP web interface under the automation section
-misp_verifycert = False
